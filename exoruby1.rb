b6460e2ee8ee370f7927393dfcdb071662ebeb2c
kayaneuh <n.kaliane@gmail.com>
2017-07-11 01:06:26 +0200

    exoruby1.rb a été modifié et finalisé pour envoi sur github

---
+++ exoruby1.rb
@@ -0,0 +1,12 @@
+def multiples
+  (1..999).select do |i|
+    i % 3 == 0 || i % 5 == 0
+  end
+end
+
+def sumMultiples(multiples)
+  total = multiples.inject(&:+)
+end
+
+tableau = [sumMultiples(multiples)]
+print tableau
